9c8ba525003d03686d070840efa7288b9bedd192
Ruslan Nikolaev <biscvi@gmail.com>
2026-01-04 00:58:21 +0300

    Added a website header, created a logo, and created a website navigation menu.

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,58 @@
-const Header = () => {
-  return <header>HEADER</header>
+import './Header.scss'
+import Logo from '@/components/Logo'
+import classNames from 'classnames'
+
+const Header = (props) => {
+  const {
+    url,
+  } = props
+
+  const menuItems = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'HTML',
+      href: '/html-tag',
+    },
+    {
+      label: 'CSS',
+      href: '/css-attributes',
+    },
+    {
+      label: 'JavaScript',
+      href: '/javascript',
+    },
+    {
+      label: 'Contacts',
+      href: '/contacts',
+    },
+
+  ]
+
+  return (
+    <header className="header">
+      <div className="header__inner container">
+        <Logo loading="eager" />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(({ label, href}, index) => (
+              <li className="header__menu-item" key={index}>
+                <a className={classNames('header__menu-link', {
+                  'is-active': href === url
+                })}
+                   href={href}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Header
